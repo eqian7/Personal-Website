c9278a5dc7ba59c8a8229f4bc7cd3a9033172226
Eric <eric.qian.d@gmail.com>
2020-06-21 15:32:38 -0500

    Changes to package

---
+++ build/precache-manifest.e49a148c3ecb7707982a5cab1529fa43.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6094f6868ffdc8666d9bd64b01ad573",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ef0c358397b8bcf85b0",
+    "url": "/static/css/main.e5f282d7.chunk.css"
+  },
+  {
+    "revision": "3a32c60b227b91324c85",
+    "url": "/static/js/2.0f886538.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.0f886538.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ef0c358397b8bcf85b0",
+    "url": "/static/js/main.251124cb.chunk.js"
+  },
+  {
+    "revision": "84850cb36c91be836092",
+    "url": "/static/js/runtime-main.33b55d74.js"
+  }
+]);
