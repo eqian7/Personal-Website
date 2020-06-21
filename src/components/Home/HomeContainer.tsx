33ba631ec98ef554daa89a1324e1b9a71170c3f9
Eric <eric.qian.d@gmail.com>
2020-06-21 15:09:19 -0500

    Move to react app

---
+++ src/components/Home/HomeContainer.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './HomeContainer.css';
+
+const HomeContainer = () => {
+  return (
+    <div id = 'HomeContainer'>
+      <div id = 'HomeText'>
+        Hi, I'm Eric
+      </div>
+    </div>
+  )
+}
+
+export default HomeContainer
